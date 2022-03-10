baa9faf2e44a9290399fd19e9707d46715e6c02f
heavens <heavens420@163.com>
2022-03-10 15:13:37 +0800

    other

---
+++ http-request/src/main/java/com/zlx/http/HttpRequestApplication.java
@@ -0,0 +1,13 @@
+package com.zlx.http;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpRequestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HttpRequestApplication.class, args);
+    }
+
+}
